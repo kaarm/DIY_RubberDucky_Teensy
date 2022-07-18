fdaab5b354a64fee6a6dbde468fbc6951d484dd6
kaarm <101365647+kaarm@users.noreply.github.com>
2022-07-18 18:21:52 +0200

    Update enum.ps1

--- enum.ps1
+++ enum.ps1
@@ -15,35 +15,35 @@ whost "Windows Enumeration Script v 0.1
 
 $standard_commands = [ordered]@{
 
-    'Basic System Information'                    = 'Start-Process "systeminfo" -NoNewWindow -Wait';
-    'Environment Variables'                       = 'Get-ChildItem Env: | ft Key,Value';
-    'Network Information'                         = 'Get-NetIPConfiguration | ft InterfaceAlias,InterfaceDescription,IPv4Address';
-    'DNS Servers'                                 = 'Get-DnsClientServerAddress -AddressFamily IPv4 | ft';
-    'ARP cache'                                   = 'Get-NetNeighbor -AddressFamily IPv4 | ft ifIndex,IPAddress,LinkLayerAddress,State';
-    'Routing Table'                               = 'Get-NetRoute -AddressFamily IPv4 | ft DestinationPrefix,NextHop,RouteMetric,ifIndex';
-    'Network Connections'                         = 'Start-Process "netstat" -ArgumentList "-ano" -NoNewWindow -Wait | ft';
-    'Connected Drives'                            = 'Get-PSDrive | where {$_.Provider -like "Microsoft.PowerShell.Core\FileSystem"}| ft';
-    'Firewall Config'                             = 'Start-Process "netsh" -ArgumentList "firewall show config" -NoNewWindow -Wait | ft';
-    'Current User'                                = 'Write-Host $env:UserDomain\$env:UserName';
-    'User Privileges'                             = 'start-process "whoami" -ArgumentList "/priv" -NoNewWindow -Wait | ft';
-    'Local Users'                                 = 'Get-LocalUser | ft Name,Enabled,LastLogon';
-    'Logged in Users'                             = 'Start-Process "qwinsta" -NoNewWindow -Wait | ft';
-    'Credential Manager'                          = 'start-process "cmdkey" -ArgumentList "/list" -NoNewWindow -Wait | ft'
-    'User Autologon Registry Items'               = 'Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\WinLogon" | select "Default*" | ft';
-    'Local Groups'                                = 'Get-LocalGroup | ft Name';
-    'Local Administrators'                        = 'Get-LocalGroupMember Administrateurs | ft Name, PrincipalSource';
-    'User Directories'                            = 'Get-ChildItem C:\Users | ft Name';
-    'Searching for SAM backup files'              = 'Test-Path %SYSTEMROOT%\repair\SAM ; Test-Path %SYSTEMROOT%\system32\config\regback\SAM';
-    'Running Processes'                           = 'gwmi -Query "Select * from Win32_Process" | where {$_.Name -notlike "svchost*"} | Select Name, Handle, @{Label="Owner";Expression={$_.GetOwner().User}} | ft -AutoSize';
-    'Installed Software Directories'              = 'Get-ChildItem "C:\Program Files", "C:\Program Files (x86)" | ft Parent,Name,LastWriteTime';
-    'Software in Registry'                        = 'Get-ChildItem -path Registry::HKEY_LOCAL_MACHINE\SOFTWARE | ft Name';
-    'Folders with Everyone Permissions'           = 'Get-ChildItem "C:\Program Files\*", "C:\Program Files (x86)\*" | % { try { Get-Acl $_ -EA SilentlyContinue | Where {($_.Access|select -ExpandProperty IdentityReference) -match "Everyone"} } catch {}} | ft';
-    'Folders with BUILTIN\User Permissions'       = 'Get-ChildItem "C:\Program Files\*", "C:\Program Files (x86)\*" | % { try { Get-Acl $_ -EA SilentlyContinue | Where {($_.Access|select -ExpandProperty IdentityReference) -match "BUILTIN\Users"} } catch {}} | ft';
-    'Checking registry for AlwaysInstallElevated' = 'Test-Path -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\Installer" | ft';
-    'Unquoted Service Paths'                      = 'gwmi -class Win32_Service -Property Name, DisplayName, PathName, StartMode | Where {$_.StartMode -eq "Auto" -and $_.PathName -notlike "C:\Windows*" -and $_.PathName -notlike ''"*''} | select PathName, DisplayName, Name | ft';
-    'Scheduled Tasks'                             = 'Get-ScheduledTask | where {$_.TaskPath -notlike "\Microsoft*"} | ft TaskName,TaskPath,State';
-    'Tasks Folder'                                = 'Get-ChildItem C:\Windows\Tasks | ft';
-    'Startup Commands'                            = 'Get-CimInstance Win32_StartupCommand | select Name, command, Location, User | fl';
+    'Basic System Information'                    = 'Start-Process "systeminfo" -NoNewWindow -Wait >> C:/rendu.txt';
+    'Environment Variables'                       = 'Get-ChildItem Env: | ft Key,Value >> C:/rendu.txt';
+    'Network Information'                         = 'Get-NetIPConfiguration | ft InterfaceAlias,InterfaceDescription,IPv4Address >> C:/rendu.txt';
+    'DNS Servers'                                 = 'Get-DnsClientServerAddress -AddressFamily IPv4 | ft >> C:/rendu.txt';
+    'ARP cache'                                   = 'Get-NetNeighbor -AddressFamily IPv4 | ft ifIndex,IPAddress,LinkLayerAddress,State >> C:/rendu.txt';
+    'Routing Table'                               = 'Get-NetRoute -AddressFamily IPv4 | ft DestinationPrefix,NextHop,RouteMetric,ifIndex >> C:/rendu.txt';
+    'Network Connections'                         = 'Start-Process "netstat" -ArgumentList "-ano" -NoNewWindow -Wait | ft >> C:/rendu.txt';
+    'Connected Drives'                            = 'Get-PSDrive | where {$_.Provider -like "Microsoft.PowerShell.Core\FileSystem"}| ft >> C:/rendu.txt';
+    'Firewall Config'                             = 'Start-Process "netsh" -ArgumentList "firewall show config" -NoNewWindow -Wait | ft >> C:/rendu.txt';
+    'Current User'                                = 'Write-Host $env:UserDomain\$env:UserName >> C:/rendu.txt';
+    'User Privileges'                             = 'start-process "whoami" -ArgumentList "/priv" -NoNewWindow -Wait | ft >> C:/rendu.txt';
+    'Local Users'                                 = 'Get-LocalUser | ft Name,Enabled,LastLogon >> C:/rendu.txt';
+    'Logged in Users'                             = 'Start-Process "qwinsta" -NoNewWindow -Wait | ft >> C:/rendu.txt';
+    'Credential Manager'                          = 'start-process "cmdkey" -ArgumentList "/list" -NoNewWindow -Wait | ft >> C:/rendu.txt'
+    'User Autologon Registry Items'               = 'Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\WinLogon" | select "Default*" | ft >> C:/rendu.txt';
+    'Local Groups'                                = 'Get-LocalGroup | ft Name >> C:/rendu.txt';
+    'Local Administrators'                        = 'Get-LocalGroupMember Administrateurs | ft Name, PrincipalSource >> C:/rendu.txt';
+    'User Directories'                            = 'Get-ChildItem C:\Users | ft Name >> C:/rendu.txt';
+    'Searching for SAM backup files'              = 'Test-Path %SYSTEMROOT%\repair\SAM ; Test-Path %SYSTEMROOT%\system32\config\regback\SAM >> C:/rendu.txt';
+    'Running Processes'                           = 'gwmi -Query "Select * from Win32_Process" | where {$_.Name -notlike "svchost*"} | Select Name, Handle, @{Label="Owner";Expression={$_.GetOwner().User}} | ft -AutoSize >> C:/rendu.txt';
+    'Installed Software Directories'              = 'Get-ChildItem "C:\Program Files", "C:\Program Files (x86)" | ft Parent,Name,LastWriteTime >> C:/rendu.txt';
+    'Software in Registry'                        = 'Get-ChildItem -path Registry::HKEY_LOCAL_MACHINE\SOFTWARE | ft Name >> C:/rendu.txt';
+    'Folders with Everyone Permissions'           = 'Get-ChildItem "C:\Program Files\*", "C:\Program Files (x86)\*" | % { try { Get-Acl $_ -EA SilentlyContinue | Where {($_.Access|select -ExpandProperty IdentityReference) -match "Everyone"} } catch {}} | ft >> C:/rendu.txt';
+    'Folders with BUILTIN\User Permissions'       = 'Get-ChildItem "C:\Program Files\*", "C:\Program Files (x86)\*" | % { try { Get-Acl $_ -EA SilentlyContinue | Where {($_.Access|select -ExpandProperty IdentityReference) -match "BUILTIN\Users"} } catch {}} | ft >> C:/rendu.txt';
+    'Checking registry for AlwaysInstallElevated' = 'Test-Path -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\Installer" | ft >> C:/rendu.txt';
+    'Unquoted Service Paths'                      = 'gwmi -class Win32_Service -Property Name, DisplayName, PathName, StartMode | Where {$_.StartMode -eq "Auto" -and $_.PathName -notlike "C:\Windows*" -and $_.PathName -notlike ''"*''} | select PathName, DisplayName, Name | ft >> C:/rendu.txt';
+    'Scheduled Tasks'                             = 'Get-ScheduledTask | where {$_.TaskPath -notlike "\Microsoft*"} | ft TaskName,TaskPath,State >> C:/rendu.txt';
+    'Tasks Folder'                                = 'Get-ChildItem C:\Windows\Tasks | ft >> C:/rendu.txt';
+    'Startup Commands'                            = 'Get-CimInstance Win32_StartupCommand | select Name, command, Location, User | fl >> C:/rendu.txt';
     
 }
 function RunCommands($commands) {
